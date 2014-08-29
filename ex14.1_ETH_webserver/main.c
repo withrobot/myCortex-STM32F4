e37581095dc51bdd8bc616fa9e6a37349d911106
irmus <irmusy@withrobot.com>
2014-08-30 08:43:06 +0900

    ex14.1 Ethernet Web Server 예제 추가

---
+++ ex14.1_ETH_webserver/main.c
@@ -0,0 +1,129 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    31-July-2013
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4x7_eth.h"
+#include "netconf.h"
+#include "main.h"
+#include "httpd.h"
+#include "serial_debug.h"
+#include "led.h"
+#include "llio.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define SYSTEMTICK_PERIOD_MS  10
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+__IO uint32_t LocalTime = 0; /* this variable is used to create a time reference incremented by 10ms */
+uint32_t timingdelay;
+
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured to
+       168 MHz, this is done through SystemInit() function which is called from
+       startup file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file
+     */
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+
+#ifdef SERIAL_DEBUG
+  DebugComPort_Init();
+
+  printf("web server example\r\n");
+
+#endif
+
+  /*Initialize Leds */
+  LED_Init();
+
+
+  /* configure ethernet (GPIOs, clocks, MAC, DMA) */
+  ETH_BSP_Config();
+
+  /* Initilaize the LwIP stack */
+  LwIP_Init();
+
+  /* Http webserver Init */
+  httpd_init();
+
+  /* Infinite loop */
+  while (1)
+  {
+    __WFI();
+    /* check if any packet received */
+    if (ETH_CheckFrameReceived())
+    {
+      /* process received ethernet packet */
+      LwIP_Pkt_Handle();
+    }
+    /* handle periodic timers for LwIP */
+    LwIP_Periodic_Handle(LocalTime);
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nCount: number of 10ms periods to wait for.
+  * @retval None
+  */
+void Delay(uint32_t nCount)
+{
+  /* Capture the current local time */
+  timingdelay = LocalTime + nCount;
+
+  /* wait until the desired delay finish */
+  while(timingdelay > LocalTime)
+  {
+  }
+}
+
+/**
+  * @brief  Updates the system local time
+  * @param  None
+  * @retval None
+  */
+void Time_Update(void)
+{
+  LocalTime += SYSTEMTICK_PERIOD_MS;
+}
+
+
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ ex14.1_ETH_webserver/serial_debug.c
@@ -0,0 +1,83 @@
+/**
+  ******************************************************************************
+  * @file    serial_debug.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    31-July-2013
+  * @brief   This file provide functions to retarget the C library printf function
+  *          to the USART.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+#include "serial_debug.h"
+#include <stdio.h>
+#include "llio.h"
+
+#ifdef SERIAL_DEBUG
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#ifdef __GNUC__
+  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+     set to 'Yes') calls __io_putchar() */
+  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Initialize COM1 interface for serial debug
+  * @note   COM1 interface is defined in stm3210g_eval.h file (under Utilities\STM32_EVAL\STM324xG_EVAL)
+  * @param  None
+  * @retval None
+  */
+void DebugComPort_Init(void)
+{
+    LLIO_Init(115200);
+}
+
+#if 0
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART */
+  USART_SendData(EVAL_COM1, (uint8_t) ch);
+
+  /* Loop until the end of transmission */
+  while (USART_GetFlagStatus(EVAL_COM1, USART_FLAG_TC) == RESET)
+  {}
+
+  return ch;
+}
+#endif
+#endif /* SERIAL_DEBUG */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
